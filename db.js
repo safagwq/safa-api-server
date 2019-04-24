4cf16349fa9e3d21fd5c18918765c1014f152ae6
safagwq <1439325342@qq.com>
2019-04-24 17:27:09 +0800

    init

---
+++ db.js
@@ -0,0 +1,18 @@
+module.exports={
+
+    fruits : [
+        {
+            id : 1,
+            name : "apple1",
+            price : 10,
+        },
+    ],
+
+    users : [
+        {     
+            id : 1,
+            username : 'safa',
+            password : '123123',
+        }
+    ]
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server')
+const db = require('./db.js')
+const server = jsonServer.create()
+const router = jsonServer.router(db)
+
+const middlewares = jsonServer.defaults()
+
+server
+.use(middlewares)
+.use(jsonServer.bodyParser)
+.use((req, res, next) => {
+    console.log('请求方法 : '+req.method , '请求地址 : '+req.url)
+
+    next()
+})
+.use(router)
+.listen(3000, () => {
+    console.log('http://localhost:3000')
+})
